8984dbd65fc200adaaaa790104548150ceb6ef47
iwgx <wahabiputra@gmail.com>
2019-04-23 16:28:18 +0700

    Adjust footer responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,14 +14,14 @@ class Footer extends Component {
       <footer className="section-navbars fixed-bottom">
         <div id="footer">
           <div className="navigation">
-            <Navbar className="navbar-transparent" expand="lg">
+            <Navbar className="navbar-transparent" expand="sm">
               <Container>
-                <div className="navbar-translate">
+                <div className="w-100 d-flex justify-content-center justify-content-sm-between">
                   <NavbarBrand href="https://iwgx.github.io/" target="_blank">
                     <i className="far fa-copyright" /> Ilham Wahabi 2019
                   </NavbarBrand>
                 </div>
-                <Nav className="ml-auto" navbar>
+                <Nav className="ml-auto d-none d-sm-flex" navbar>
                   <NavItem>
                     <NavLink
                       href="https://twitter.com/ilhamwahabigx"
