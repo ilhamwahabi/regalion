714a20c95c9d86de20141bcfac34d49cc1da7c0f
iwgx <wahabiputra@gmail.com>
2019-04-23 16:17:19 +0700

    Add navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar />
         <Footer />
       </div>
     );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Navbar, Container, Input } from "reactstrap";
+
+class ComponentsNavbar extends Component {
+  render() {
+    return (
+      <Navbar className="fixed-top navbar-transparent" expand="lg">
+        <Container>
+          <Input 
+            type="text" name="pokemon" placeholder="Search any Pokemon" 
+            style={{ textAlign: 'center', fontSize: '16px' }}
+          />
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default ComponentsNavbar;
