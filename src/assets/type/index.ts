63a35288a1ed30df5572db148272a18b8cc2d004
iwgx <wahabiputra@gmail.com>
2019-04-24 07:21:45 +0700

    Add conditional to check color

---
+++ src/assets/type/index.ts
@@ -0,0 +1,21 @@
+type pokemonType =
+  | "Bug"
+  | "Dark"
+  | "Dragon"
+  | "Electric"
+  | "Fairy"
+  | "Fighting"
+  | "Fire"
+  | "Flying"
+  | "Ghost"
+  | "Grass"
+  | "Ground"
+  | "Ice"
+  | "Normal"
+  | "Poison"
+  | "Psychic"
+  | "Rock"
+  | "Steel"
+  | "Water";
+
+export default pokemonType;
--- src/components/Pokedex/Description.tsx
+++ src/components/Pokedex/Description.tsx
@@ -1,5 +1,69 @@
 import React from "react";
 import { Row, Col, Button } from "reactstrap";
+import pokemonType from "../../assets/type";
+
+const renderType = (type: pokemonType) => {
+  let color;
+
+  switch (type) {
+    case "Bug":
+      color = "#A8B820";
+      break;
+    case "Dark":
+      color = "#705848";
+      break;
+    case "Dragon":
+      color = "#7038F8";
+      break;
+    case "Electric":
+      color = "#F8D030";
+      break;
+    case "Fairy":
+      color = "#EE99AC";
+      break;
+    case "Fighting":
+      color = "#C03028";
+      break;
+    case "Fire":
+      color = "#f5365c";
+      break;
+    case "Flying":
+      color = "#A890F0";
+      break;
+    case "Ghost":
+      color = "#705898";
+      break;
+    case "Grass":
+      color = "#78C850";
+      break;
+    case "Ground":
+      color = "#E0C068";
+      break;
+    case "Ice":
+      color = "#98D8D8";
+      break;
+    case "Normal":
+      color = "#A8A878";
+      break;
+    case "Poison":
+      color = "#A040A0";
+      break;
+    case "Psychic":
+      color = "#F85888";
+      break;
+    case "Rock":
+      color = "#B8A038";
+      break;
+    case "Steel":
+      color = "#B8B8D0";
+      break;
+    case "Water":
+      color = "#5e72e4";
+      break;
+  }
+
+  return <Button color="info">{type}</Button>;
+};
 
 const Description = ({
   types,
@@ -23,7 +87,7 @@ const Description = ({
       <Row className="mb-3">
         {types.map((type, index) => (
           <Col key={index} className="text-center">
-            <Button color="info">{type}</Button>
+            {renderType(type as pokemonType)}
           </Col>
         ))}
       </Row>
