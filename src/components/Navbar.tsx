9f175ae8232ff56a11eda55174de08b016f3af85
iwgx <wahabiputra@gmail.com>
2019-04-27 15:02:50 +0700

    Create api and configure form

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
-import { Navbar, Container, Input } from "reactstrap";
+import { Navbar, Container, Input, Form, FormGroup } from "reactstrap";
 
 class ComponentsNavbar extends Component {
+  state = {
+    search: ""
+  };
+
+  onChangeSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ search: event.target.value });
+  };
+
+  onSearchPokemon = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(this.state.search, "Submitted!!!");
+  };
+
   render() {
     return (
       <Navbar className="navbar-transparent" expand="lg">
         <Container>
-          <Input
-            type="text"
-            name="pokemon"
-            placeholder="Search any Pokemon"
-            style={{ textAlign: "center", fontSize: "16px" }}
-          />
+          <Form className="w-100" onSubmit={this.onSearchPokemon}>
+            <FormGroup>
+              <Input
+                type="text"
+                name="pokemon"
+                placeholder="Search any Pokemon"
+                style={{ textAlign: "center", fontSize: "16px" }}
+                value={this.state.search}
+                onChange={this.onChangeSearch}
+              />
+            </FormGroup>
+          </Form>
         </Container>
       </Navbar>
     );
