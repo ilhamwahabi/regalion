592b48ec3cb2ede043ad31751e06ce7762d700f8
iwgx <wahabiputra@gmail.com>
2019-04-30 00:49:35 +0700

    Show backdrop when load pokemon

---
+++ src/components/Loading/Backdrop.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Backdrop = () => <div style={{ backgroundColor: '#212121', opacity: 0.5, position: "fixed", width: '100vw', height: '100%' }} />;
+
+export default Backdrop;
--- src/components/Loading/Spinner.tsx
+++ src/components/Loading/Spinner.tsx
@@ -3,15 +3,21 @@ import { css } from "@emotion/core";
 import { PulseLoader } from "react-spinners";
 import { connect } from "react-redux";
 
+import Backdrop from "./Backdrop";
+
 const override = css`
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  z-index: 10;
 `;
 
 const Spinner = ({ loading }: { loading: boolean }) => (
-  <PulseLoader css={override as any} loading={loading} color="white" />
+  <>
+    <PulseLoader css={override as any} loading={loading} color="white" />
+    { loading && <Backdrop /> }
+  </>
 );
 
 const mapStateToProps = ({ loading }: { loading: boolean }) => {
