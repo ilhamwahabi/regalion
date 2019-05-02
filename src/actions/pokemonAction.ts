7e513c7640e11f41913dd84522307beaba589702
iwgx <wahabiputra@gmail.com>
2019-05-03 01:05:11 +0700

    Reset form after search success

--- src/actions/pokemonAction.ts
+++ src/actions/pokemonAction.ts
@@ -1,11 +1,14 @@
+import { reset } from "redux-form";
 import api from "../api";
 
 export const changePokemon = (pokemonName: string) => async (
   dispatch: Function
 ) => {
   dispatch({ type: "START_LOADING" });
-  const response = await api.get(`/${pokemonName}`);
 
+  const response = await api.get(`/${pokemonName}`);
   dispatch({ type: "CHANGE_POKEMON", payload: response.data });
+
+  dispatch(reset("searchPokemon"));
   dispatch({ type: "FINISH_LOADING" });
 };
