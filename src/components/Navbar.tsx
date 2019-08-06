ff37248f1fb889e27310b4fee084a40b1952c0f6
Ilham Wahabi <wahabiputra@gmail.com>
2019-08-06 07:27:59 +0700

    change navbar styling with emotion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,26 +2,52 @@ import React, { Component } from "react";
 import { Navbar, Container, Input, Form, FormGroup } from "reactstrap";
 import { Field, reduxForm, InjectedFormProps } from "redux-form";
 import { connect } from "react-redux";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 
 import { changePokemon } from "../actions";
 import { pokemonNames } from "../assets/ts/name";
+import { IPalettes } from "../types";
 
-class ComponentsNavbar extends Component<InjectedFormProps, {}> {
+interface INavbarProps {
+  name: string;
+  palettes: IPalettes;
+  changePokemon(pokemonNameOrIndex: number | string): void;
+}
+
+class ComponentsNavbar extends Component<
+  INavbarProps & InjectedFormProps<{}, INavbarProps>
+> {
   onSearchPokemon = ({ search }: any) => {
     if (
       pokemonNames.map(p => p.toLowerCase()).includes(search) &&
-      (this.props as any).name.toLowerCase() !== search
+      this.props.name.toLowerCase() !== search
     )
-      (this.props as any).changePokemon(search);
+      this.props.changePokemon(search);
   };
 
   renderField = ({ input, meta }: any) => {
+    const { palettes } = this.props;
+
+    const borderColor = meta.valid
+      ? `rgb(${palettes.lightMuted})`
+      : `rgb(${palettes.darkVibrant})`;
+
     return (
       <Input
         type="text"
         name="pokemon"
         placeholder="Search any Pokémon"
-        style={{ textAlign: "center", fontSize: "16px" }}
+        spellCheck={false}
+        css={css`
+          text-align: center;
+          font-size: 16px;
+          color: rgb(${palettes.lightMuted});
+          border-color: ${borderColor} !important;
+          &::placeholder {
+            color: rgb(${palettes.lightMuted});
+          }
+        `}
         autoComplete="off"
         invalid={meta.active && !!meta.error}
         list="pokemons"
@@ -58,7 +84,7 @@ const validate = ({ search }: any) => {
     return { search: "Invalid Pokémon Name" };
 };
 
-const formWrapper = reduxForm({
+const formWrapper = reduxForm<{}, INavbarProps>({
   form: "searchPokemon",
   validate: validate as any
 })(ComponentsNavbar);
@@ -66,7 +92,10 @@ const formWrapper = reduxForm({
 const mapStateToProps = (state: any) => {
   const { pokemon, selectedForm } = state.pokemon;
 
-  return { name: pokemon[selectedForm].name };
+  return {
+    name: pokemon[selectedForm].name,
+    palettes: pokemon[selectedForm].palettes
+  };
 };
 
 export default connect(
