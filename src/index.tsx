114ff38a999e7006ad382459d4514666e7db4520
ilhamwahabi <wahabiputra@gmail.com>
2026-05-24 16:20:03 +0700

    uninstall splitbee

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import splitbee from '@splitbee/web';
 
 import "assets/scss/index.scss";
 
 import App from "./components/App";
 import { store, persistor } from "./configureStore";
 import LoadingScreen from "./components/Loading";
-
-splitbee.init({
-  scriptUrl: "/bee.js",
-  apiUrl: "/_hive",
-})
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
